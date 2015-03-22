c5bd8b233b321f861e1fda76bf9fb2632b407aa3
dharmatejus <R.dtejus@gmail.com>
2015-03-22 10:47:35 -0700

    Added code to the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix() and cacheSolve() work together to 
+## optimize access to an inverted matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Call makeCacheMatrix() with an invertible square matrix. This will
+## return a list of functions get and set input matrix and inverted matrix
 
+makeCacheMatrix <- function(mat = matrix()) {
+  imat <- matrix()
+  set <- function(y) {
+    if (!identical (mat,y) ) {
+      mat <<- y 
+      imat <<- matrix()   }
+  }
+  get <- function() mat
+  setInv <- function(nuInv) imat <<- nuInv
+  getInv <- function() imat
+  
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Call cacheSolve() with an object returned by makeCacheMatrix() to obtain
+## the inverse of teh matrix passed to makeCacheMatrix(). If the inversion
+## had already been done, with an earlier call, then the cached inverse is
+## returned saving potentially expeensive/time-consimung computation. If
+## the fucntion is being called for the first time using a given matrix then
+## it computes the inverse using solve(), returns and caches the result
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (mfl, ...) {
+  
+  cimat <- mfl$getInv()      # get the cached invert
+  # Is invert empty? this matrix has not been inverted yet? 
+  # If so invert and set the cache.
+  if (identical(cimat, matrix())) {  
+    cmat <- mfl$get()
+    cimat <- solve(cmat)
+    mfl$setInv(cimat) }
+  else {
+    message( "Getting from cache")
+  }
+  cimat
 }
+
